2a19eb289bbefb26c4fe6de5481d9dec9ae8253c
Sesa26 <sesadlamini@gmail.com>
2021-10-01 04:16:46 +0200

    hjg

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,21 +2,14 @@
 	<div class="home">
 		<div>
 			<b-navbar type="dark" variant="dark">
-				<b-navbar-nav>
-					<b-nav-item href="#">Home</b-nav-item>
-
-					<!-- Navbar dropdowns -->
-					<b-nav-item-dropdown text="Lang" right>
-						<b-dropdown-item href="#">EN</b-dropdown-item>
-						<b-dropdown-item href="#">ES</b-dropdown-item>
-						<b-dropdown-item href="#">RU</b-dropdown-item>
-						<b-dropdown-item href="#">FA</b-dropdown-item>
-					</b-nav-item-dropdown>
-
-					<b-nav-item-dropdown text="User" right>
-						<b-dropdown-item href="#">Account</b-dropdown-item>
-						<b-dropdown-item href="#">Settings</b-dropdown-item>
-					</b-nav-item-dropdown>
+				<b-navbar-nav class="ml-auto" right>
+					<b-nav-form>
+						<b-button size="sm" v-b-modal.modal-1 class="my-2 my-sm-0">login</b-button>
+						<b-modal id="modal-1" title="Login" @ok="login">
+							<b-form-input type="email" class="my-2" v-model="phoneNumber" placeholder="Email"></b-form-input>
+							<b-form-input type="password" class="my-4" v-model="OTP" placeholder="password"></b-form-input>
+						</b-modal>
+					</b-nav-form>
 				</b-navbar-nav>
 			</b-navbar>
 		</div>
@@ -27,5 +20,19 @@
 export default {
 	name: "Home",
 	components: {},
+	data() {
+		return {
+			email: "",
+			password: "",
+			hasSentSMS: false,
+			OTP: "",
+		};
+	},
+	methods: {
+		
+		login() {
+			console.log("clicked")
+		},
+	},
 };
 </script>
